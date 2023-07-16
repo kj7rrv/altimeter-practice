1add5325a4b70111fbd63c5995509ebed2e68494
Samuel Sloniker <sam@kj7rrv.com>
2023-07-16 12:53:24 -0700

    dejquerify

--- js/ui.js
+++ js/ui.js
@@ -11,100 +11,63 @@ By Sébastien Matton (seb_matton@hotmail.com)
 Published under GPLv3 License.
 */
 
-(function($) {
+function Altimeter(placeholder) {
+    var built = true,
+    settings = {
+        size : 400,
+        altitude : 0,
+        pressure : 29.92,
+        img_directory : 'img/'
+    } 
 
-    "use strict";
+    // Altimeter - Set altitude
+    function _setAltitude(altitude){
+        var hand100 = altitude / 1000 * 360
+        var hand1000 = altitude / 10000 * 360
+        var hand10000 = altitude / 100000 * 360
+        placeholder.querySelector('div.altimeter_hand100').style['transform'] = 'rotate(' + hand100 + 'deg)'
+        placeholder.querySelector('div.altimeter_hand1000').style['transform'] = 'rotate(' + hand1000 + 'deg)'
+        placeholder.querySelector('div.altimeter_hand10000').style['transform'] = 'rotate(' + hand10000 + 'deg)'
+    }
 
-    function Altimeter( placeholder, options ) {
+    // Altimeter - Set pressure
+    function _setPressure(pressure, milibar){
 
-        var built = true,
-        settings = $.extend({
-            size : 400,
-            altitude : 0,
-            pressure : 30,
-            img_directory : 'img/'
-            }, options 
-        )
+        var pressure1 = 0, pressure2 = 0
+        // pressure1: 5 units = 9 degrees
+        // pressure2: 0.1 units = 6 degrees
 
-        // Altimeter - Set altitude
-        function _setAltitude(altitude){
-            var hand100 = altitude / 1000 * 360;
-            var hand1000 = altitude / 10000 * 360;
-            var hand10000 = altitude / 100000 * 360;
-            placeholder.each(function(){
-                $(this).find('div.instrument.altimeter div.altimeter_hand100').css('transform', 'rotate(' + hand100 + 'deg)');
-                $(this).find('div.instrument.altimeter div.altimeter_hand1000').css('transform', 'rotate(' + hand1000 + 'deg)');
-                $(this).find('div.instrument.altimeter div.altimeter_hand10000').css('transform', 'rotate(' + hand10000 + 'deg)');
-            });    
+        if (pressure >= 27.1 && pressure <= 33) {
+            pressure1 = (925 - 33.8639 * pressure) * 1.8
+            pressure2 = (pressure - 27.1) * 60
         }
 
-        // Altimeter - Set pressure (by default inHg; set milibar to true if you wish to use mbar)
-        function _setPressure(pressure, milibar){
+        placeholder.querySelector('div.altimeter_pressurembar').style['transform'] = 'rotate(' + pressure1 + 'deg)'
+        placeholder.querySelector('div.altimeter_pressureinhg').style['transform'] = 'rotate(' + -pressure2 + 'deg)'
+    }
 
-            var pressure1 = 0, pressure2 = 0;
-            // pressure1: 5 units = 9 degrees
-            // pressure2: 0.1 units = 6 degrees
+    // Set size of instrument
+    function _resize(size){
+        var sz = (size < 100) ? sz = 100 : sz = size
+        placeholder.querySelector("div.instrument").style["height"] = sz + "px"
+        placeholder.querySelector("div.instrument").style["width"] = sz + "px"
+    }
 
-            if (milibar !== true) {
-                if (pressure >= 27.1 && pressure <= 33) {
-                    pressure1 = (925 - 33.8639 * pressure) * 1.8;
-                    pressure2 = (pressure - 27.1) * 60;
-                }
-            } else {
-                if (pressure >= 925 && pressure <= 1120) {
-                    pressure1 = (925 - pressure) * 1.8;
-                    pressure2 = (pressure * 0.0295300 - 27.1) * 60;
-                }
-            }
+    // Creation of the instrument
+    placeholder.innerHTML = '<div class="instrument altimeter"><div class="indicator_background"><img src="' + settings.img_directory + 'indicator_background_dashboard.svg" class="box" alt="" /></div><div class="indicator_background_screws"><img src="' + settings.img_directory + 'indicator_background_screws.svg" class="box" alt="" /></div><div class="indicator_inner"><div class="altimeter_pressureinhg box"><img src="' + settings.img_directory + 'altimeter_pressure_inhg.svg" class="box" alt="" /></div><div class="altimeter_pressurembar box"><img src="' + settings.img_directory + 'altimeter_pressure_mbar.svg" class="box" alt="" /></div><div class="altimeter_background box"><img src="' + settings.img_directory + 'altimeter_background.svg" class="box" alt="" /></div><div class="altimeter_hand10000 box"><img src="' + settings.img_directory + 'altimeter_hand_10000ft.svg" class="box" alt="" /></div><div class="altimeter_foreground box"><img src="' + settings.img_directory + 'altimeter_foreground.svg" class="box" alt="" /></div><div class="altimeter_hand1000 box"><img src="' + settings.img_directory + 'altimeter_hand_1000ft.svg" class="box" alt="" /></div><div class="altimeter_hand100 box"><img src="' + settings.img_directory + 'altimeter_hand_100ft.svg" class="box" alt="" /></div></div><div class="indicator_foreground"><img src="' + settings.img_directory + 'indicator_foreground.svg" class="box" alt="" /></div></div>'
+    _setAltitude(settings.altitude)
+    _setPressure(settings.pressure)
 
-            placeholder.each(function(){
-                $(this).find('div.instrument.altimeter div.altimeter_pressurembar').css('transform', 'rotate(' + pressure1 + 'deg)');
-            });    
-            placeholder.each(function(){
-                $(this).find('div.instrument.altimeter div.altimeter_pressureinhg').css('transform', 'rotate(' + -pressure2 + 'deg)');
-            });            
+    _resize(settings.size);
 
-        }
+    // Public methods
+    this.setAltitude = function(altitude){_setAltitude(altitude)}
+    this.setPressure = function(pressure, milibar){_setPressure(pressure, milibar)}
+    this.resize = function(size){_resize(size)}
+    return built
+}
 
-        // Set size of instrument
-        function _resize(size){
-            var sz = (size < 100) ? sz = 100 : sz = size;
-            placeholder.each(function(){
-                $(this).find('div.instrument').css({height : sz, width : sz});
-            });
-        }
-
-        // Creation of the instrument
-        placeholder.each(function(){
-            $(this).html('<div class="instrument altimeter"><div class="indicator_background"><img src="' + settings.img_directory + 'indicator_background_dashboard.svg" class="box" alt="" /></div><div class="indicator_background_screws"><img src="' + settings.img_directory + 'indicator_background_screws.svg" class="box" alt="" /></div><div class="indicator_inner"><div class="altimeter_pressureinhg box"><img src="' + settings.img_directory + 'altimeter_pressure_inhg.svg" class="box" alt="" /></div><div class="altimeter_pressurembar box"><img src="' + settings.img_directory + 'altimeter_pressure_mbar.svg" class="box" alt="" /></div><div class="altimeter_background box"><img src="' + settings.img_directory + 'altimeter_background.svg" class="box" alt="" /></div><div class="altimeter_hand10000 box"><img src="' + settings.img_directory + 'altimeter_hand_10000ft.svg" class="box" alt="" /></div><div class="altimeter_foreground box"><img src="' + settings.img_directory + 'altimeter_foreground.svg" class="box" alt="" /></div><div class="altimeter_hand1000 box"><img src="' + settings.img_directory + 'altimeter_hand_1000ft.svg" class="box" alt="" /></div><div class="altimeter_hand100 box"><img src="' + settings.img_directory + 'altimeter_hand_100ft.svg" class="box" alt="" /></div></div><div class="indicator_foreground"><img src="' + settings.img_directory + 'indicator_foreground.svg" class="box" alt="" /></div></div>');
-            _setAltitude(settings.altitude);
-            _setPressure(settings.pressure);
-
-            _resize(settings.size);
-        });
-
-        // Public methods
-        this.setAltitude = function(altitude){_setAltitude(altitude);}
-        this.setPressure = function(pressure, milibar){_setPressure(pressure, milibar);}
-        this.resize = function(size){_resize(size);}
-        return built;
-    };
-
-    // Extension to jQuery
-    $.altimeter = function(placeholder, options){
-        var altimeter = new Altimeter($(placeholder), options);
-        return altimeter;
-    };
-
-    $.fn.altimeter = function(data, options){
-        return this.each(function(){
-            $.altimeter(this, options);
-        });
-    };
-
-}( jQuery ));
-
-const altimeter = $.altimeter('#altimeter')
+const altimeter = new Altimeter(document.querySelector("#altimeter"))
 const input = document.querySelector("#input")
 const button = document.querySelector("#button")
 
@@ -114,7 +77,6 @@ function update(min, max, inc) {
     return alt
 }
 
-altimeter.setPressure(29.92)
 let altitude = update(0, 18000, 10)
 
 button.addEventListener("click", function(e){
